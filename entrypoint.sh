7576aa9be46d7685f5957a403a0e07e2eefa68af
lingharto <lingharto@gmail.com>
2023-11-22 14:52:46 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ function failedSignal() {
   RESPONSE_FAILED=$(curl --silent -H 'content-type: application/json' -H "Authorization: Bearer $PHP_SCAN_AUTH_TOKEN" -G $ENDPOINT_FAILED)
 }
 
-function successSignal() {
-  ENDPOINT_SUCCESS="https://api.phpscan.com/api/check/ci_success/$1"
-  CMD="curl --silent --location "$ENDPOINT_SUCCESS" --header "Authorization: Bearer $PHP_SCAN_AUTH_TOKEN"
-  RESPONSE_SUCCESS=$(curl --silent --location "$ENDPOINT_SUCCESS" --header "Authorization: Bearer $PHP_SCAN_AUTH_TOKEN")
-}
+#function successSignal() {
+#  ENDPOINT_SUCCESS="https://api.phpscan.com/api/check/ci_success/$1"
+#  CMD="curl --silent --location "$ENDPOINT_SUCCESS" --header "Authorization: Bearer $PHP_SCAN_AUTH_TOKEN"
+#  RESPONSE_SUCCESS=$(curl --silent --location "$ENDPOINT_SUCCESS" --header "Authorization: Bearer $PHP_SCAN_AUTH_TOKEN")
+#}
 
 function successSignal() {
   ENDPOINT_SUCCESS="https://api.phpscan.com/api/check/ci_success/$1"
